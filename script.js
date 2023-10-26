3f5d210c586f74d09f461d88d37fb8f3169fb457
AshRossel <ashrossel@gmail.com>
2023-10-26 23:00:53 +0100

    Add game round function

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function getComputerChoice() {
       return "Scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  // Convert string selections to title case
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection.replace(playerSelection.charAt(0), 
+  playerSelection.charAt(0).toUpperCase());
+
+  computerSelection = computerSelection.toLowerCase();
+  computerSelection = computerSelection.replace(computerSelection.charAt(0), 
+  computerSelection.charAt(0).toUpperCase());
+
+  // Create the result round messages
+  let winMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+  let loseMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
+  let drawMessage = "You draw! Both choices are equal";
+
+  // Determine who wins the round
+  if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return winMessage;
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return winMessage;
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return winMessage;
+  } else if (playerSelection === computerSelection) {
+    return drawMessage;
+  } else {
+    return loseMessage;
+  }
+}
